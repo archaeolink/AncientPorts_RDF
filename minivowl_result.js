e24a6d9ad0a85dfd9eb70dd61de1aa1177322f1d
situx <situx@users.noreply.github.com>
2023-05-06 00:07:28 +0000

    Deploying to gh-pages from @ archaeolink/AncientPorts_RDF@6aa2434c8616b3ea985076217e5ecefbd08e26a5 🚀

---
+++ minivowl_result.js
@@ -0,0 +1,43 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "Harbour",
+   "type": "class",
+   "uri": "http://www.ancientports.com/ont#Harbour"
+  },
+  {
+   "name": "Point",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/sf#Point"
+  },
+  {
+   "name": "Class",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Class"
+  },
+  {
+   "name": "ObjectProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
+  },
+  {
+   "name": "DatatypeProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
+  }
+ ],
+ "links": [
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "hasGeometry",
+   "propertyTo": "class",
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+  }
+ ]
+}
