20843eb31cc6af1447f0034778e8ff1e65db6c39
situx <situx@users.noreply.github.com>
2023-05-06 16:24:15 +0000

    Deploying to gh-pages from @ archaeolink/AncientPorts_RDF@45cec783fd2df1806fbc6c9f6f40c60afbbbf6f6 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -6,14 +6,14 @@ var minivowlresult={
  ],
  "nodes": [
   {
-   "name": "Harbour",
-   "type": "class",
-   "uri": "http://www.ancientports.com/ont#Harbour"
-  },
-  {
    "name": "Point",
    "type": "class",
    "uri": "http://www.opengis.net/ont/sf#Point"
+  },
+  {
+   "name": "Harbour",
+   "type": "class",
+   "uri": "http://www.ancientports.com/ont#Harbour"
   },
   {
    "name": "ObjectProperty",
@@ -21,20 +21,44 @@ var minivowlresult={
    "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
   },
   {
-   "name": "Class",
+   "name": "Person",
    "type": "class",
-   "uri": "http://www.w3.org/2002/07/owl#Class"
+   "uri": "http://xmlns.com/foaf/0.1/Person"
+  },
+  {
+   "name": "Article",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/Article"
   },
   {
    "name": "DatatypeProperty",
    "type": "class",
    "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
+  },
+  {
+   "name": "Class",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Class"
   }
  ],
  "links": [
   {
-   "source": 0,
-   "target": 1,
+   "source": 4,
+   "target": 3,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 1,
+   "target": 3,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 1,
+   "target": 0,
    "valueTo": "hasGeometry",
    "propertyTo": "class",
    "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
