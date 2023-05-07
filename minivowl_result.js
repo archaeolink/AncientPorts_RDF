d74bb7eba8c93862ac8a333fd7cb61b83c805df9
situx <situx@users.noreply.github.com>
2023-05-07 13:25:35 +0000

    Deploying to gh-pages from @ archaeolink/AncientPorts_RDF@cd86ae0ab0f713a40376063003b7db2c0b09dd9b 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -11,34 +11,19 @@ var minivowlresult={
    "uri": "http://www.ancientports.com/ont#Harbour"
   },
   {
-   "name": "Point",
-   "type": "class",
-   "uri": "http://www.opengis.net/ont/sf#Point"
-  },
-  {
-   "name": "Person",
-   "type": "class",
-   "uri": "http://xmlns.com/foaf/0.1/Person"
-  },
-  {
    "name": "Article",
    "type": "class",
    "uri": "http://purl.org/ontology/bibo/Article"
   },
   {
-   "name": "Class",
-   "type": "class",
-   "uri": "http://www.w3.org/2002/07/owl#Class"
-  },
-  {
-   "name": "Proceedings",
-   "type": "class",
-   "uri": "http://purl.org/ontology/bibo/Proceedings"
-  },
-  {
    "name": "BookSection",
    "type": "class",
    "uri": "http://purl.org/ontology/bibo/BookSection"
+  },
+  {
+   "name": "Book",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/Book"
   },
   {
    "name": "Thesis",
@@ -51,65 +36,115 @@ var minivowlresult={
    "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
   },
   {
-   "name": "Book",
-   "type": "class",
-   "uri": "http://purl.org/ontology/bibo/Book"
-  },
-  {
    "name": "ObjectProperty",
    "type": "class",
    "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
+  },
+  {
+   "name": "Point",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/sf#Point"
+  },
+  {
+   "name": "Person",
+   "type": "class",
+   "uri": "http://xmlns.com/foaf/0.1/Person"
+  },
+  {
+   "name": "Class",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Class"
+  },
+  {
+   "name": "Proceedings",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/Proceedings"
   }
  ],
  "links": [
   {
    "source": 0,
-   "target": 1,
+   "target": 7,
    "valueTo": "hasGeometry",
    "propertyTo": "class",
    "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   },
   {
-   "source": 6,
-   "target": 2,
+   "source": 3,
+   "target": 8,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 9,
-   "target": 2,
+   "source": 4,
+   "target": 8,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 10,
+   "target": 8,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
    "source": 0,
-   "target": 2,
+   "target": 8,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 3,
-   "target": 2,
+   "source": 2,
+   "target": 8,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 7,
-   "target": 2,
+   "source": 1,
+   "target": 8,
    "valueTo": "creator",
    "propertyTo": "class",
    "uriTo": "http://purl.org/dc/elements/1.1/creator"
   },
   {
-   "source": 5,
-   "target": 2,
-   "valueTo": "creator",
+   "source": 0,
+   "target": 3,
+   "valueTo": "isReferencedBy",
    "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 4,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 10,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 2,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
+  },
+  {
+   "source": 0,
+   "target": 1,
+   "valueTo": "isReferencedBy",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/terms/isReferencedBy"
   }
  ]
 }
