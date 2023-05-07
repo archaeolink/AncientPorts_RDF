c8046e0db9b08bb6b940f905a96a2bb4271c39c2
situx <situx@users.noreply.github.com>
2023-05-07 11:26:18 +0000

    Deploying to gh-pages from @ archaeolink/AncientPorts_RDF@56a16f6dc8ad8ec3c9c705dbbbc935ced1fe795f 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -6,19 +6,14 @@ var minivowlresult={
  ],
  "nodes": [
   {
-   "name": "Point",
-   "type": "class",
-   "uri": "http://www.opengis.net/ont/sf#Point"
-  },
-  {
    "name": "Harbour",
    "type": "class",
    "uri": "http://www.ancientports.com/ont#Harbour"
   },
   {
-   "name": "ObjectProperty",
+   "name": "Point",
    "type": "class",
-   "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
+   "uri": "http://www.opengis.net/ont/sf#Point"
   },
   {
    "name": "Person",
@@ -31,37 +26,90 @@ var minivowlresult={
    "uri": "http://purl.org/ontology/bibo/Article"
   },
   {
+   "name": "Class",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Class"
+  },
+  {
+   "name": "Proceedings",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/Proceedings"
+  },
+  {
+   "name": "BookSection",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/BookSection"
+  },
+  {
+   "name": "Thesis",
+   "type": "class",
+   "uri": "http://purl.org/ontology/bibo/Thesis"
+  },
+  {
    "name": "DatatypeProperty",
    "type": "class",
    "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
   },
   {
-   "name": "Class",
+   "name": "Book",
    "type": "class",
-   "uri": "http://www.w3.org/2002/07/owl#Class"
+   "uri": "http://purl.org/ontology/bibo/Book"
+  },
+  {
+   "name": "ObjectProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
   }
  ],
  "links": [
   {
-   "source": 4,
-   "target": 3,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 1,
-   "target": 3,
-   "valueTo": "creator",
-   "propertyTo": "class",
-   "uriTo": "http://purl.org/dc/elements/1.1/creator"
-  },
-  {
-   "source": 1,
-   "target": 0,
+   "source": 0,
+   "target": 1,
    "valueTo": "hasGeometry",
    "propertyTo": "class",
    "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+  },
+  {
+   "source": 6,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 9,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 0,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 3,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 7,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
+  },
+  {
+   "source": 5,
+   "target": 2,
+   "valueTo": "creator",
+   "propertyTo": "class",
+   "uriTo": "http://purl.org/dc/elements/1.1/creator"
   }
  ]
 }
